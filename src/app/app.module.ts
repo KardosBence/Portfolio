df3531f5d9591ae9bf0a479142dae8f7c3854bee
kardosb <kardbence@gmail.com>
2023-03-17 18:08:43 +0100

    V1.0.0. alap fő és error oldal létrehozva

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { HibaComponent } from './hiba/hiba.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    HibaComponent
   ],
   imports: [
     BrowserModule,
